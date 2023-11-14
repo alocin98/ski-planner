1aaf17f57179bb5f80d35c9a06a66327f560edc1
Nicolas Müller <nicolasmueller@MBPvonNicolas2.yallo.box>
2023-11-14 14:52:06 +0100

    fix docker

--- api/handlers/config.go
+++ api/handlers/config.go
@@ -9,10 +9,14 @@ import (
 
 // DotEnvVariable -> get .env
 func DotEnvVariable(key string) string {
+	// Check if the application is running in a Docker environment
+	if os.Getenv("DOCKER_ENV") == "true" {
+		// In Docker, do not load .env file
+		return os.Getenv(key)
+	}
 
-	// load .env file
+	// Load .env file if not in Docker environment
 	err := godotenv.Load("./.env")
-
 	if err != nil {
 		log.Fatalf("Error loading .env file: %s", err.Error())
 	}
