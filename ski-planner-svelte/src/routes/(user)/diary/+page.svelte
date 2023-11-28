d2a48b645b546d6db5cd1f2aceaa8ade303a50b5
Nicolas Müller <nicolasmueller@MacBook-Pro-von-Nicolas-2.local>
2023-11-28 23:34:08 +0100

    beautiful trainings

--- ski-planner-svelte/src/routes/(user)/diary/+page.svelte
+++ ski-planner-svelte/src/routes/(user)/diary/+page.svelte
@@ -6,5 +6,15 @@
 </script>
 
 <h1 class="text-3xl mb-6">Diary</h1>
-{JSON.stringify(data.trainings)}
+<div class="grid grid-cols-1 gap-6 md:grid-cols-2">
+	{#each data.trainings as activity}
+		<div class="bg-white rounded-lg shadow-md p-6" key={activity.trainingId}>
+			<h2 class="text-xl font-semibold mb-2">{activity.Name}</h2>
+			<p class="text-gray-600 mb-2">Distance: {activity.Distance} meters</p>
+			<p class="text-gray-600 mb-2">Moving Time: {activity.MovingTime} seconds</p>
+			<p class="text-gray-600 mb-2">Elevation Gain: {activity.TotalElevationGain} meters</p>
+			<!-- Add other key details here -->
+		</div>
+	{/each}
+</div>
 <NoEntries />
