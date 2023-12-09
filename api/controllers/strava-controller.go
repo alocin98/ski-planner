a7121b7fb026419a58e195f1080205c4e7ec4798
Nicolas Müller <nicolas.mueller2@gmail.com>
2023-12-09 23:32:50 +0100

    fix

--- api/controllers/strava-controller.go
+++ api/controllers/strava-controller.go
@@ -16,7 +16,7 @@ var clientSecret = "e3cfc4a374bedf06e5e10fe7302c935a3ec28e65"
 var clientId = "39719"
 
 func StravaAuthorize(response http.ResponseWriter, request *http.Request, _ httprouter.Params) {
-	url := "https://www.strava.com/oauth/authorize?client_id=39719&response_type=code&redirect_uri=http://localhost:8080/api/strava/exchange-token&approval_prompt=force&scope=activity:read_all"
+	url := "https://www.strava.com/oauth/authorize?client_id=39719&response_type=code&redirect_uri=https://skiyeti.io/api/strava/exchange-token&approval_prompt=force&scope=activity:read_all"
 	http.Redirect(response, request, url, http.StatusSeeOther)
 }
 
